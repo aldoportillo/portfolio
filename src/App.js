eeda71ec0f28cc7578c03f622168dfeb3c11cb75
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-31 16:41:57 -0500

    Divide main content

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <Header />
       <div className='main-content'>
-        <h2>Hello, my name is Aldo Portillo!</h2>
-
-        <p> I am a software developer who specializes in the MERN stack. Aside from coding I enjoy bartending and training in MMA.</p>
-        
-        <p>Check out <a href="#">my projects</a>, <a href="#">my blogs</a>, and <a href="#">my sparring footage</a>. </p>
-
-        <img src={require('./media/redrocks.png')} />
-
+        <div className='intro'>
+          <h2>Hello, my name is Aldo Portillo!</h2>
+          <p> I am a software developer who specializes in the MERN stack. Aside from coding I enjoy bartending and training in MMA.</p>         
+          <p>Check out <a href="#">my projects</a>, <a href="#">my blogs</a>, and <a href="#">my sparring footage</a>. </p>
+        </div>
+        <div className='main-image'>
+          <img src={require('./media/redrocks.png')} />
+        </div>
       </div>
       <Footer />
     </div>
