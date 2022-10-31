1b84988f863363f8bd128dc6ff7acf8e69bbdef8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-31 16:10:59 -0500

    Create header with nav and links

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Aldo Portillo</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  )
+}
