7f76a1384ec924936f92c6443d6e489ee8324f01
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-31 16:33:16 -0500

    Create basic homepage layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* I will add routes and render them here */}
+      <div className='main-content'>
+        <h2>Hello, my name is Aldo Portillo!</h2>
+
+        <p> I am a software developer who specializes in the MERN stack. Aside from coding I enjoy bartending and training in MMA.</p>
+        
+        <p>Check out <a href="#">my projects</a>, <a href="#">my blogs</a>, and <a href="#">my sparring footage</a>. </p>
+
+        <img src={require('./media/redrocks.png')} />
+
+      </div>
       <Footer />
     </div>
   );
