21075200e711888557545f0e5931226782958c4b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-02 16:21:48 -0500

    Add fake data

---
+++ server/fakeData.js
@@ -0,0 +1,27 @@
+export const blogs = [
+    {
+        name: "title 1",
+        content: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "blank for now",
+        date: "today"
+    },
+    {
+        name: "title 2",
+        content: "2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "blank for now",
+        date: "today"
+    },
+    {
+        name: "title 3",
+        content: "3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "blank for now",
+        date: "today"
+    },
+    {
+        name: "title 4",
+        content: "4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        img: "blank for now",
+        date: "today"
+    },
+
+]
