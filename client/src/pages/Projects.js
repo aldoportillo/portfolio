9aa2c0606989964a82af1b4246a421f9e35f853e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-02 17:10:46 -0500

    Render instances of Project card component

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Project from '../components/Project'
 
-export default function Projects() {
+export default function Projects(props) {
+
+  //I will map through projects data to return Project component cards
   return (
-    <div>Projects</div>
+    <div className='projects-page'>
+      <Project name="BJJ Score Tracker" img={require('../media/projects/bjj-score.png')} />
+      <Project name="Rock, Paper, Scissors" img={require('../media/projects/rockpaperscissors.png')} />
+      <Project name="Tenzies" img={require('../media/projects/tenzies.png')} />
+    </div>
   )
 }
