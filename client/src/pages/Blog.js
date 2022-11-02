e2854c2b7eb4fdd38f9d7a736b555da4326efc01
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-02 15:58:06 -0500

    Render backend data on screen

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
 export default function Blog() {
+
+    const [backendData, setBackendData] = React.useState([{}])
+
+    React.useEffect(() => {
+        fetch("/api").then(
+            response => response.json()
+        ).then(
+            data => {
+                setBackendData(data)
+                console.log(backendData)
+            }
+        )
+    }, [])
+
+    
   return (
+    <>
     <div>Blog</div>
+
+     {(typeof backendData.randomArr === 'undefined') ? (
+        <p>Loading...</p>
+    ): (
+        // backendData.randomArr.map((val, i) => {
+        //     <p key={i}>{val}</p>
+        // })
+        <p>{backendData.randomArr}</p>
+    )} 
+    </>
   )
 }
