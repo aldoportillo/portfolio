5b282fb132cbcc9f2c3ef37af4a3b2e03090243c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-02 17:41:50 -0500

    Create basic form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-page'>
+        <form method='POST' className='form'>
+            <label for="first-name">First Name: </label>
+            <input type="text" id="first-name" name="first-name" required></input>
+            <label for="last-name">Last Name: </label>
+            <input type="text" id="last-name" name="last-name" required></input>
+            <label for="email">Last Name: </label>
+            <input type="email" id="email" name="email" required></input>
+            <button>Submit</button>
+        </form>
+    </div>
   )
 }
