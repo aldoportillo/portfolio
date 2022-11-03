59548c64c2d2dd71a106cb9136fc8257665ac874
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-03 16:45:08 -0500

    Create project data file to store projects

---
+++ client/src/data/projectData.js
@@ -0,0 +1,21 @@
+export default projectData = [
+    {
+        title: 'BJJ Score Tracker',
+        img: require('../media/projects/bjj-score.png'),
+        bio: 'Bio pending',
+        link: 'https://aldoportillo.github.io/BJJ-Score-Tracker/',
+    },
+    {
+        title: 'Rock, Paper, Scissors',
+        img: require('../media/projects/rockpaperscissors.png'),
+        bio: 'Bio pending',
+        link: 'https://aldoportillo.github.io/rockPaperScissors/'
+    },
+    {
+        title: 'Tenzies',
+        img: require('../media/projects/tenzies.png'),
+        bio: 'Bio pending',
+        link: 'https://aldoportillo.github.io/Tenzies/'
+    },
+
+]
