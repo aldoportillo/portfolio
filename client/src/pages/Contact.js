54310736e592044d3feac41a929a876cab57aa81
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-03 16:37:16 -0500

    Fix contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Contact() {
   return (
     <div className='contact-page'>
-        <form method='POST' className='form'>
+        <form method='POST' className='form' action='/contacts'>
             <label for="first-name">First Name: </label>
             <input type="text" id="first-name" name="first-name" required></input>
             <label for="last-name">Last Name: </label>
             <input type="text" id="last-name" name="last-name" required></input>
-            <label for="email">Last Name: </label>
+            <label for="email">E-mail: </label>
             <input type="email" id="email" name="email" required></input>
             <button>Submit</button>
         </form>
