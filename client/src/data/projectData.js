e5a0bcdd07ad7e608e01d6fb6f62221b6c595a00
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-03 17:37:36 -0500

    Add Message Node App data

--- client/src/data/projectData.js
+++ client/src/data/projectData.js
@@ -1,21 +1,27 @@
-export default projectData = [
+export const projectData = [
     {
         title: 'BJJ Score Tracker',
         img: require('../media/projects/bjj-score.png'),
-        bio: 'Bio pending',
+        bio: 'A Brazilian Jiu Jitsu scoring app. Track your takedowns, positions, and submission points with a timer.',
         link: 'https://aldoportillo.github.io/BJJ-Score-Tracker/',
     },
     {
         title: 'Rock, Paper, Scissors',
         img: require('../media/projects/rockpaperscissors.png'),
-        bio: 'Bio pending',
+        bio: 'A rock, paper, scissors game against a computer. First one to five wins!',
         link: 'https://aldoportillo.github.io/rockPaperScissors/'
     },
     {
         title: 'Tenzies',
         img: require('../media/projects/tenzies.png'),
-        bio: 'Bio pending',
+        bio: 'A React game based on rolling die until all are the same number. Can you get a perfect score?',
         link: 'https://aldoportillo.github.io/Tenzies/'
     },
+    {
+        title: 'Message Node',
+        img: require('../media/projects/messageNode.png'),
+        bio: 'A messaging application similar to a forum with an Express backend and MongoDB database.',
+        link: 'https://message-node-ap.herokuapp.com/',
+    }
 
 ]
