e0e377a3e2b1ac4279b3f06a1279e43f374d12de
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-05 18:05:47 -0500

    Create custom page when a blog title is clicked

--- client/src/components/BlogList.js
+++ client/src/components/BlogList.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import BlogEntry from './BlogEntry'
 
 export default function BlogList({title, src}) {
   return (
-    <div>{title}</div>
+    <div className='blog-list'>
+        <Routes>
+            <Route path={`/${title}`} element={<BlogEntry title={title} />} />
+        </Routes>
+        
+        <Link to={`/${title}`}>{title}</Link>
+    </div>
   )
 }
