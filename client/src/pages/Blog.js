9e1116a2b473fe3191a44fcd1343dcf8d17857c2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-05 17:43:54 -0500

    Comment out API call and conditional rendering to work on rendering blogs

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,33 +1,38 @@
 import React from 'react'
+import BlogList from '../components/BlogList'
 
 export default function Blog() {
 
-    const [backendData, setBackendData] = React.useState([{}])
+    const [backendData, setBackendData] = React.useState([{title: 'blog1'}, {title: 'blog2'}, {title: 'blog3'}])
 
-    React.useEffect(() => {
-        fetch("/api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-                console.log(backendData)
-            }
+    console.log(backendData)
+
+    // React.useEffect(()=> {
+    //     fetch('/api')
+    //     .then(res => res.json())
+    //     .then(data => setBackendData(data)) 
+   
+    // }, [])
+
+    console.log(backendData)
+
+     const renderBlogs = backendData.map( blog => {
+        return(
+            <BlogList title={blog.title}/>
         )
-    }, [])
+     })
 
-    
   return (
     <>
     <div>Blog</div>
 
-     {(typeof backendData.randomArr === 'undefined') ? (
+    {renderBlogs}
+     {/* {(typeof backendData.randomArr === 'undefined') ? (
         <p>Loading...</p>
     ): (
-        // backendData.randomArr.map((val, i) => {
-        //     <p key={i}>{val}</p>
-        // })
-        <p>{backendData.randomArr}</p>
-    )} 
+        // <p>{backendData.randomArr}</p>
+        {renderBlogs}
+    )}  */}
     </>
   )
 }
