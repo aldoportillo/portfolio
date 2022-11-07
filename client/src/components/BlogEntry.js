7ceba904111be2cc01b2e9dfb0fe8cffc9e631c2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-07 16:20:25 -0600

    Render image or paragraph depending on the data passed into content array

--- client/src/components/BlogEntry.js
+++ client/src/components/BlogEntry.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function BlogEntry({title, content, imgArr}) {
-    console.log(imgArr)
+export default function BlogEntry({title, content }) {
+
+  const renderContent = content.map( type => {
+    if( type.includes('jpeg')){
+      return <img src={type} />
+    } else if (typeof(type) === 'string') {
+      return <p>{type}</p>
+      
+    }
+  })
   return (
     <div className='blog-entry'>
         <h3>{title}</h3>
-        <p>{content}</p>
-        <img src={imgArr[1]} alt="" /> 
-        
+        <div>{renderContent}</div>
         <Link to="/blog"><button>Back to blogs page</button></Link>
     </div>
 
--- client/src/data/blogData.js
+++ client/src/data/blogData.js
@@ -26,7 +26,15 @@ export const blogData = [
     {
         title: 'Abandoned Mansion',
         date: 'June 8, 2022',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        images: [require('../data/blogImageData/abandonedMansion/IMG_3863.jpeg'), require('../data/blogImageData/abandonedMansion/IMG_3866.jpeg'), require('../data/blogImageData/abandonedMansion/IMG_3877.jpeg'), require('../data/blogImageData/abandonedMansion/IMG_3878.jpeg')]
+        content: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            require('../data/blogImageData/abandonedMansion/IMG_3863.jpeg'),
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            require('../data/blogImageData/abandonedMansion/IMG_3866.jpeg'),
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            require('../data/blogImageData/abandonedMansion/IMG_3877.jpeg'), 
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            require('../data/blogImageData/abandonedMansion/IMG_3878.jpeg')
+            ],
     }
 ]
