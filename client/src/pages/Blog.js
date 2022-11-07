2706f9adbb9bd22630882fcd110bee59d519b719
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-06 21:25:40 -0600

    Remove backend call from Blog.js

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import BlogList from '../components/BlogList'
 import { nanoid } from 'nanoid'
-import { blogData } from '../data/blogData'
 
 
-export default function Blog() {
+export default function Blog({blogData}) {
 
     const [backendData, setBackendData] = React.useState(blogData)
 
-    console.log(backendData)
-
-    // React.useEffect(()=> {
-    //     fetch('/api')
-    //     .then(res => res.json())
-    //     .then(data => setBackendData(data)) 
-   
-    // }, [])
 
     const renderBlogs = backendData.map( blog => {
         return(
