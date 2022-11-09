29b39e26607c2fa711956586fbcd48ad47283ed2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-09 16:27:47 -0600

    Create createContact controller

---
+++ server/controllers/portfolioControllers.js
@@ -0,0 +1,14 @@
+const Contact = require("../models/contact")
+
+const createContact = async (req, res) => {
+    try{
+        const contact = await Contact.create(req.body)
+        res.status(200).json(contact)
+    } catch (err) {
+        res.status(500).json({msg: err.message})
+    }
+}
+
+module.exports = {
+    createContact,
+}
