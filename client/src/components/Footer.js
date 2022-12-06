025ef8e7d76c305b2122bb443cf2bf94e5cc9e6a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-06 14:04:06 -0600

    Add footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <a href="https://whimsical.com/detailed-site-map-2W9iQ5URBfbF7csc8sASAe" target={"_blank"} rel="noreferrer">Site Map</a>
+      <p>Aldo Portillo &copy; 2022</p>
+    </footer>
   )
 }
