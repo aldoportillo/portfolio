e08edc25e53aff50d6c8ef502579195e80a638fd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-05 19:07:09 -0600

    Add Header and footer to page container

--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
 
-export default function PageContainer() {
+export default function PageContainer({children}) {
   return (
-    <div>PageContainer</div>
+    <div className="container">
+        <Header />
+        {children}
+        <Footer />
+    </div>
   )
 }
