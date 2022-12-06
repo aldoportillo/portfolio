cb55feee742932bf62ba9e0e8f31093b018c8bd9
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-06 14:43:11 -0600

    Implement hamburger menu and conditionally render nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,16 +5,20 @@ import { Sling as Hamburger } from 'hamburger-react'
 export default function Header() {
 
   const [isOpen, setIsOpen] = React.useState(false)
-  
+
+  function toggleMenu() {
+
+  }
   return (
     <header>
       <h1>Aldo Portillo</h1>
-      <nav>
+      {isOpen && <nav>
         <Link to="/"><h5>Home</h5></Link>
         <Link to="/projects"><h5>Projects</h5></Link>
         <Link to="/blog"><h5>Blog</h5></Link>
-        <Link to="/contact"><h5>Contact</h5></Link>
-      </nav>
+        <Link to="/contact"><h5>Contact</h5></Link> </nav>
+      }
+      <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} onToggle={toggleMenu()}/>
     </header>
   )
 }
