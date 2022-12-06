ff77e93156e53dee073830018b23ca1934d90272
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-05 20:25:10 -0600

    Add blog entry

--- client/src/data/blogData.js
+++ client/src/data/blogData.js
@@ -41,4 +41,32 @@ export const blogDataAPI = [
             require('../data/blogImageData/abandonedMansion/IMG_3878.jpeg')
             ],
     },
+    {
+        title: 'Grappling Tournament',
+        date: 'November 2022',
+        content: [
+            'I competed in my second grappling tournament November 2022. I had completely forgotten a week prior, so I had a week to prepare and drop down to 149 lbs. I knew once I cut down, I had the win. The day started off pretty normal. I weighed in and made weight. I proceeded to go to Panera and eat three sandwiches thinking I had two hours to digest. Unfortunately, my first match was pushed forward by an hour, so I went into it with a full stomach.',
+            require('../data/blogImageData/grapplingTournament/1.PNG'),
+            'Here I was trying to wrestle my opponent with a full stomach, so I gave up and tried to pull guard to play off my back. Unfortunately, my opponent took mount, and scored points but beyond that, I felt his weight on my full stomach. I knew I had to get out of that position in fear of throwing up.',
+            require('../data/blogImageData/grapplingTournament/2.PNG'),
+            'Luckily, I was able to get to guard, and I hit him with an up and over sweep to trick him into posting his arm out and possibly get a kimura. Due to the far post, I switched my gameplan to going for a trinagle choke. I snuck my leg in through the gap, and I was able to lock it in.',
+            require('../data/blogImageData/grapplingTournament/2.5.PNG'),
+            "The fight for the trinagle was difficult. My coach kept on insisting I control the leg even though I am used to just pulling the head in. But my opponent's defense made me realize why controlling the leg was more impotant. My opponent's coach kept on telling him to smash my face and to press on my stomach, and I asked him politely to not press on my stomach. After 25 seconds of fighting for the triangle, my opponent tapped leading to my first win of the day in 110 seconds.",
+            require('../data/blogImageData/grapplingTournament/3.PNG'),
+            "I proceeded to my second and final match. This match meant a lot more to me. My second opponent had a match with one of my favorite teammates a month prior and he ripped his ear about a quarter off. I knew I had to win, but knowing my teammate usually gives me a hard time at practice and knowing that my opponent was giving him a hard time during their match, I started to doubt myself. The match started very similar to the previous one. I wanted to pull goard because of my full stomach.",
+            require('../data/blogImageData/grapplingTournament/4.PNG'),
+            "Within the first 35 seconds, my opponent attempted an ankle lock. This attack is considered a submission from 'The Dark Arts' in BJJ. A submission not many people get to drill or experience beacuse of the danger behind leg attacks. Luckily, a training partner of mine loves going for this submission, so I was ready to defend it and attempt an armbar. Unfortunately, the match was paused as soon as I went for the armbar counter because we went out of bounds. I knew I was screwed.",
+            require('../data/blogImageData/grapplingTournament/5.PNG'),
+            "After another 30 seconds of wrestling after we restarted, I attempted a belly down armbar, but I messed it up by rolling to the wrong side. It was a rookie mistake, but I knew I had a chance. I was super active, and I was moving smoothly.",
+            require('../data/blogImageData/grapplingTournament/6.PNG'),
+            "My opponent attempted another ankle lock, but I realized that he was looking for a panic tap since it was poorly set up. I was able to defend it without a problem, and this is where the scramble began. The scamble was about 100 seconds ending in an arm in guillotine attempt from myself. I looked at the scoreboard and realized I was down 2-7. I knew I had to get this submission.",
+            require('../data/blogImageData/grapplingTournament/7.PNG'),
+            "My opponent ended up escaping the guillotine, and I was ready to give up. I layed down for 5 seconds until I remembered what happened to my teammate. I took a deep breath and shot up a triangle with 15 seconds left. My opponent's coach kept on telling him to not tap, so I knew I had to put him to sleep. With 4 seconds left, I decided to increase the choke to win by TKO; however, it takes an everage of 6 seconds to put someone to sleep. With two seconds left before my opponent's nap, the timer ended I rushed to look at the score board, and I saw 8-7. A win by points. I thanked my opponent for the match and went to get my medal.",
+            require('../data/blogImageData/grapplingTournament/8.PNG'),
+            "After the win, I had a list of new things I wanted to work on, and I was excited to get back to the gym to work on those. I can't wait for my next tournament.",
+            require('../data/blogImageData/grapplingTournament/9.PNG'),
+            
+
+        ]
+    }
 ]
