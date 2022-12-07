a2d2d6a7b05db2a23026b00722ddc4d4895de527
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-06 20:00:08 -0600

    Move hamburger to pageContainer

--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import { Link } from 'react-router-dom'
+
+
 
 export default function PageContainer({children}) {
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  function toggleMenu() {
+
+  }
+
   return (
     <div className="container">
-        <Header />
+        <Header isOpen={isOpen} setIsOpen={setIsOpen} toggleMenu={toggleMenu}/>
+        {isOpen && <nav>
+        <Link to="/"><h5>Home</h5></Link>
+        <Link to="/projects"><h5>Projects</h5></Link>
+        <Link to="/blog"><h5>Blog</h5></Link>
+        <Link to="/contact"><h5>Contact</h5></Link> </nav>
+      }
         {children}
         <Footer />
     </div>
