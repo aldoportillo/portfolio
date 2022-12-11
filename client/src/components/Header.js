746447bc245bf5b6b737835cfc59eccdeb5e60a5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 23:01:03 -0600

    Conditionally render dropdown or nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
+import SideBar from './SideBar'
 
-export default function Header({isOpen, toggleMenu, setIsOpen}) {
+export default function Header({isOpen, toggleMenu, setIsOpen, windowSize}) {
 
   return (
     <header>
       <h1>Aldo Portillo</h1>
-      <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} onToggle={toggleMenu()}/>
+      { windowSize >= 1025 &&<SideBar />}
+      {windowSize < 1025 && <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} onToggle={toggleMenu()}/>}
     </header>
   )
 }
--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -8,15 +8,34 @@ import SideBar from './SideBar'
 
 export default function PageContainer({children}) {
 
+  const getWindowSize = () => {
+    const {innerWidth} = window;
+    return innerWidth
+}
+
   const [isOpen, setIsOpen] = React.useState(false)
+
+  const [windowSize, setWindowSize] = React.useState(getWindowSize())
 
   function toggleMenu() {
 
   }
 
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowSize(getWindowSize())
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <div className="container">
-        <Header isOpen={isOpen} setIsOpen={setIsOpen} toggleMenu={toggleMenu}/>
+        <Header isOpen={isOpen} setIsOpen={setIsOpen} toggleMenu={toggleMenu} windowSize={windowSize}/>
         <div className='main'>
           {children}
           {isOpen && <SideBar isOpen={isOpen} setIsOpen={setIsOpen}/>}
