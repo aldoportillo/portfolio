ebec9771729a15645bd957686e234de957d388bd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-11 16:22:58 -0600

    Add more projects

--- client/src/data/projectData.js
+++ client/src/data/projectData.js
@@ -1,9 +1,27 @@
 export const projectData = [
     {
+        title: 'Neat on the Rocks',
+        img: require('../media/projects/neatOnTheRocks.png'),
+        bio: 'A calorie and macronutrient calculator for cocktails including a fake e-commerce site',
+        link: 'https://www.neatonthe.rocks/macro',
+    },
+    {
         title: 'BJJ Score Tracker',
         img: require('../media/projects/bjj-score.png'),
-        bio: 'A Brazilian Jiu Jitsu scoring app. Track your takedowns, positions, and submission points with a timer.',
-        link: 'https://aldoportillo.github.io/BJJ-Score-Tracker/',
+        bio: 'A Brazilian Jiu Jitsu scoring app. Track your takedowns, positions, and submission points with a timer',
+        link: 'https://www.bjjscore.me/',
+    },
+    {
+        title: 'Memory Card Game',
+        img: require('../media/projects/memoryCard.png'),
+        bio: 'A memory card game using a regular deck of cards. Can you get through all the levels?',
+        link: 'https://memory-card-three.vercel.app/',
+    },
+    {
+        title: 'Sudoku Validator',
+        img: require('../media/projects/sudokuValidator.png'),
+        bio: 'A sudoku validator using React. As soon as a correct game is played, the text in the bottom auto updates.',
+        link: 'https://sodoku-checker-ui.vercel.app/',
     },
     {
         title: 'Rock, Paper, Scissors',
@@ -20,8 +38,21 @@ export const projectData = [
     {
         title: 'Message Node',
         img: require('../media/projects/messageNode.png'),
-        bio: 'A messaging application similar to a forum with an Express backend and MongoDB database.',
+        bio: 'A messaging application similar to a forum with an Express backend and MongoDB database',
         link: 'https://message-node-ap.herokuapp.com/',
-    }
+    },
+    {
+        title: 'Selan Dice Game',
+        img: require('../media/projects/selanDiceGame.png'),
+        bio: 'A virtual verison of a game my Calculus teacher, Mr. Selan, had us play. I made the first version back in 2017 when I was taking his class',
+        link: 'https://aldoportillo.github.io/SelanDiceGame/',
+    },
+    {
+        title: 'Excuse Generator',
+        img: require('../media/projects/excuseGenerator.png'),
+        bio: 'An excuse generator for when you are running late, missed a meeting or simply need an excuse. Based off a meme I saw in 2011',
+        link: 'https://aldoportillo.github.io/ExcuseGenerator/',
+    },
+    
 
 ]
