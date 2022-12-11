8ab4a5794c4d3f9d71ff3897d9b46c950794d477
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 23:28:01 -0600

    Handle onClick nav by using useLocation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
 import SideBar from './SideBar'
 
-export default function Header({isOpen, toggleMenu, setIsOpen, windowSize}) {
+export default function Header({isOpen,  setIsOpen, windowSize}) {
 
   return (
     <header>
       <div className='h-nav'>
         <h1>Aldo Portillo</h1>
         { windowSize >= 1025 &&<SideBar />}
-        {windowSize < 1025 && <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} onToggle={toggleMenu()}/>}
+        {windowSize < 1025 && <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} />}
       </div>
       <div className='v-nav'>
         {windowSize < 1025 && isOpen && <SideBar />}
