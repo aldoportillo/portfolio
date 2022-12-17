263bbf0ae7984ee49200a9c8d0368965f3a8f46a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-17 01:50:18 -0600

    Make header title  clickable

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
 import SideBar from './SideBar'
+import { Link } from 'react-router-dom'
 
 export default function Header({isOpen,  setIsOpen, windowSize}) {
 
   return (
     <header>
       <div className='h-nav'>
-        <h1>Aldo Portillo</h1>
+        <h1><Link to="/">Aldo Portillo</Link></h1>
         { windowSize >= 1025 &&<SideBar />}
         {windowSize < 1025 && <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} />}
       </div>
