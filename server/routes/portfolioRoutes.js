e2515ee292ded3ad68d098bda6925c6f36400fc3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-17 12:39:11 -0600

    Create routes for portfolio routes

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const {getProjects, createProject} = require('../controllers/portfolioControllers')
+const {getProjects, createProject, getBlogs, createBlog} = require('../controllers/portfolioControllers')
 const router = express.Router();
 
 router.get("/api/projects", getProjects)
 router.post("/api/projects", createProject)
+
+router.get("/api/blogs", getBlogs)
+router.post("/api/blogs", createBlog)
 
 module.exports = router
