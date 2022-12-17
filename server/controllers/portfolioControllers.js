827be26bd7eba00eef6dbf640c969fd406e29c80
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-17 12:24:17 -0600

    Create get request to get projects from database

--- server/controllers/portfolioControllers.js
+++ server/controllers/portfolioControllers.js
@@ -1,8 +1,10 @@
 const Project = require("../models/projectModel")
+const asyncHandler = require('express-async-handler')
 
-const getProjects = (req, res) => {
-    res.send("<h2>ProjectsPage</h2>")
-}
+const getProjects = asyncHandler(async (req, res) => {
+    const projects = await Project.find({})
+    res.status(200).json(projects)
+})
 
 const createProject = async(req,res) => {
     const {title, img, bio, link} = req.body; //Gather info from request body
