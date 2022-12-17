4b2d4fea2c7641210eab82724306302028f45e1f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-17 01:30:46 -0600

    Remove images from front end and modify data for projects

--- client/src/data/projectData.js
+++ client/src/data/projectData.js
@@ -1,55 +1,55 @@
 export const projectData = [
     {
         title: 'Neat on the Rocks',
-        img: require('../media/projects/neatOnTheRocks.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/neatOnTheRocks_qmkqmn.png',
         bio: 'A calorie and macronutrient calculator for cocktails including a fake e-commerce site',
         link: 'https://www.neatonthe.rocks/macro',
     },
     {
         title: 'BJJ Score Tracker',
-        img: require('../media/projects/bjj-score.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/bjj-score_pokadz.png',
         bio: 'A Brazilian Jiu Jitsu scoring app. Track your takedowns, positions, and submission points with a timer',
         link: 'https://www.bjjscore.me/',
     },
     {
         title: 'Memory Card Game',
-        img: require('../media/projects/memoryCard.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/memoryCard_aa77fm.png',
         bio: 'A memory card game using a regular deck of cards. Can you get through all the levels?',
         link: 'https://memory-card-three.vercel.app/',
     },
     {
         title: 'Sudoku Validator',
-        img: require('../media/projects/sudokuValidator.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/sudokuValidator_prbd8m.png',
         bio: 'A sudoku validator using React. As soon as a correct game is played, the text in the bottom auto updates.',
         link: 'https://sodoku-checker-ui.vercel.app/',
     },
     {
         title: 'Rock, Paper, Scissors',
-        img: require('../media/projects/rockpaperscissors.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/rockpaperscissors_yv5rqg.png',
         bio: 'A rock, paper, scissors game against a computer. First one to five wins!',
         link: 'https://aldoportillo.github.io/rockPaperScissors/'
     },
     {
         title: 'Tenzies',
-        img: require('../media/projects/tenzies.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/tenzies_vqlyvu.png',
         bio: 'A React game based on rolling die until all are the same number. Can you get a perfect score?',
         link: 'https://aldoportillo.github.io/Tenzies/'
     },
     {
         title: 'Message Node',
-        img: require('../media/projects/messageNode.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/messageNode_uu6lnc.png',
         bio: 'A messaging application similar to a forum with an Express backend and MongoDB database',
         link: 'https://message-node-ap.herokuapp.com/',
     },
     {
         title: 'Selan Dice Game',
-        img: require('../media/projects/selanDiceGame.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/selanDiceGame_jd7y0f.png',
         bio: 'A virtual verison of a game my Calculus teacher, Mr. Selan, had us play. I made the first version back in 2017 when I was taking his class',
         link: 'https://aldoportillo.github.io/SelanDiceGame/',
     },
     {
         title: 'Excuse Generator',
-        img: require('../media/projects/excuseGenerator.png'),
+        img: 'https://res.cloudinary.com/dkhtrg1ts/image/upload/v1671261969/Portfolio%20-%20Projects/excuseGenerator_zzhom8.png',
         bio: 'An excuse generator for when you are running late, missed a meeting or simply need an excuse. Based off a meme I saw in 2011',
         link: 'https://aldoportillo.github.io/ExcuseGenerator/',
     },
