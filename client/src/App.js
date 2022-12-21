6f685f637fd3e9efca37a6c2ef40774d27dc3214
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-21 00:49:43 -0600

    modify blog path

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,8 @@ import Projects from './pages/Projects';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import BlogEntry from './components/BlogEntry';
-import { nanoid } from 'nanoid';
 import PageContainer from './components/PageContainer';
 import axios from 'axios';
-import env from 'react-dotenv';
 
 function App() {
 
@@ -30,14 +28,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<PageContainer children={<Home />}/>} />
         <Route path="/projects" element={<PageContainer children={<Projects />} />} />
-        <Route path="/blog/*" element={<PageContainer children={<Blog blogData={blogData}/>} />}/>
+        <Route path="/blog/" element={<PageContainer children={<Blog blogData={blogData}/>} />}/>
+        <Route path="/blog/:id" element={<PageContainer children={<BlogEntry />} />} />
         <Route path="/contact" element={<PageContainer children={<Contact />} />} />
-
-        {blogData.map(blog => {
-          return (<Route key={nanoid()} path={`/blog/${blog.title}`} exact element={
-            <PageContainer children={<BlogEntry title={blog.title} content={blog.content} imgArr={blog.images}/>}/> 
-          } />) 
-        } )}
       </Routes>
     </div>
   );
