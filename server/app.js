47baf379d53f156db52d8576d41568e32159538a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 22:35:01 -0600

    Modify port to 5000

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const portfolioRoutes = require('./routes/portfolioRoutes')
 
 const app = express()
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.MONGO_URI)
