ffca4d0597d0f71fd772c07458bf4cf8def26d18
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-21 18:31:14 -0600

    Modify HTML structure

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -7,18 +7,20 @@ export default function Blog({blogData}) {
 
     const renderBlogs = blogData.map( blog => {
         return(
+          <div className='blog-list'>
         <Link to={`/blog/${blog._id}`} state={{ from: "blog", data: blog}}>
             <h3>{blog.title}</h3>
             <p>{blog.date}</p>
         </Link>
+        </div> 
         )
     })
   return (
     <div className='blog-page'>
     <h2>Blogs</h2>
-      <div className='blog-list'>
+      
         {renderBlogs}
-      </div> 
+      
     </div>
   )
 }
