9773851f12147b29f8061ea6f376fcb92dda6cfb
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-23 16:51:29 -0600

    Modify route tree

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -2,12 +2,14 @@ const express = require("express")
 const {getProjects, createProject, getBlogs, createBlog, sendEmail} = require('../controllers/portfolioControllers')
 const router = express.Router();
 
+router.post("/api/contact", sendEmail)
+
 router.get("/api/projects", getProjects)
 router.post("/api/projects", createProject)
 
 router.get("/api/blogs", getBlogs)
 router.post("/api/blogs", createBlog)
 
-router.post("/contact", sendEmail)
+
 
 module.exports = router
