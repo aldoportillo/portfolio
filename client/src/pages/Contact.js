0cee48423a7858c06a6f5d3c756c3beb1d1b021b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-23 22:02:02 -0600

    Link Contact page to server

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,17 +1,40 @@
 import React from 'react'
 
 export default function Contact() {
+
+  const [status, setStatus] = React.useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("https://aldoportillo-portfolio.onrender.com/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+       body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+
   return (
     <div className='contact-page'>
         <h3>Contact Me!</h3>
-        <form method='POST' className='form' action='/contacts'>
-            <label for="first-name">First Name: </label>
-            <input type="text" id="first-name" name="first-name" required></input>
-            <label for="last-name">Last Name: </label>
-            <input type="text" id="last-name" name="last-name" required></input>
+        <form onSubmit={handleSubmit}>
+            <label for="name">Name: </label>
+            <input type="text" id="name" name="name" required></input>
             <label for="email">E-mail: </label>
             <input type="email" id="email" name="email" required></input>
-            <button className='button'>Submit</button>
+            <label for="message">Message: </label>
+            <input type="text" id="message" name="message" required></input>
+            <button className='button'>{status}</button>
         </form>
     </div>
   )
