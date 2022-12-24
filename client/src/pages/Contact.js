33eab29213dde1c137fe7f03a8985aea18fd451a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-23 22:20:20 -0600

    Modify to use axios

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,39 +1,46 @@
+import axios from 'axios'
 import React from 'react'
 
 export default function Contact() {
 
-  const [status, setStatus] = React.useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("https://aldoportillo-portfolio.onrender.com/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-       body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
+    const [status, setStatus] = React.useState("Submit")
+    const [name, setName] = React.useState("")
+    const [email, setEmail] = React.useState("")
+    const [message, setMessage] = React.useState("")
+
+    const data = {
+        name,
+        email, 
+        message
+    }
+
+    const sendEmail = async (e) => {
+        e.preventDefault()
+
+        setStatus("Sending...");
+
+        try {
+            await axios.post(`http://localhost:5000/api/contact`, data)
+            alert("sent")
+            setName("")
+            setEmail("")
+            setMessage("")
+            setStatus("Submit");
+        } catch (err) {
+            alert("error")
+        }
+    }
 
   return (
     <div className='contact-page'>
         <h3>Contact Me!</h3>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={sendEmail}>
             <label for="name">Name: </label>
-            <input type="text" id="name" name="name" required></input>
+            <input type="text" id="name" name="name" onChange={(e) => {setName(e.target.value)}} required></input>
             <label for="email">E-mail: </label>
-            <input type="email" id="email" name="email" required></input>
+            <input type="email" id="email" name="email" onChange={(e) => {setEmail(e.target.value)}} required></input>
             <label for="message">Message: </label>
-            <input type="text" id="message" name="message" required></input>
+            <input type="text" id="message" name="message" onChange={(e) => {setMessage(e.target.value)}}required></input>
             <button className='button'>{status}</button>
         </form>
     </div>
