7c59d416493a67ea44259daa8b7d5db16767cfb9
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-02 16:55:30 -0600

    Add key to project card renders

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -7,7 +7,7 @@ export default function Projects({projectData, isLoading}) {
 
   const renderProjects = projectData?.map(project => {
     return (
-      <Project name={project.title} img={project.img} key={project.key} link={project.link} bio={project.bio}/>
+      <Project name={project.title} img={project.img} key={project._id} link={project.link} bio={project.bio}/>
     )
   })
   return (
