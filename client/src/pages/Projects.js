14b82455b9bed691f8ecaa0556ac4595c39ec941
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-02 16:32:34 -0600

    Recieve  isLoading props and conditionally render loader

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import Loader from '../components/Loader'
 import Project from '../components/Project' 
 
-export default function Projects({projectData}) {
+export default function Projects({projectData, isLoading}) {
 
 
-  const renderProjects = projectData.map(project => {
+  const renderProjects = projectData?.map(project => {
     return (
       <Project name={project.title} img={project.img} key={project.key} link={project.link} bio={project.bio}/>
     )
   })
   return (
     <div className='projects-page'>
-      <h2>Projects</h2>
-      <div className='projects-container'>
-        {renderProjects}
-      </div>
+      {isLoading ? 
+      <Loader /> :
+      <>
+        <h2>Projects</h2>
+        <div className='projects-container'>
+          {renderProjects}
+        </div>
+      </>}
     </div>
   )
 }
