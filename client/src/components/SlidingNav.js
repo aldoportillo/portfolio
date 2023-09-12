c01cba0008993d632896eff70a3fc9377fe21494
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-12 15:42:47 -0500

    Create sliding nav

--- client/src/components/SlidingNav.js
+++ client/src/components/SlidingNav.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function SlidingNav() {
+export default function SlidingNav({navWidth}) {
   return (
-    <div>SlidingNav</div>
+    <nav className='sliding-nav' style={{"width": `${navWidth}vw`}}>
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
   )
 }
