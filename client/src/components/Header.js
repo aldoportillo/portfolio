716121c09099af2256894a61df3703c53c5d0673
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-12 15:47:56 -0500

    Use ternary opertator

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,18 +3,18 @@ import { Sling as Hamburger } from 'hamburger-react'
 import SideBar from './SideBar'
 import { Link } from 'react-router-dom'
 
-export default function Header({isOpen,  setIsOpen, windowSize}) {
+export default function Header({openNav,  setOpenNav, windowSize}) {
 
   return (
     <header>
       <div className='h-nav'>
         <h1><Link to="/">Aldo Portillo</Link></h1>
-        { windowSize >= 1025 &&<SideBar />}
-        {windowSize < 1025 && <Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={isOpen} toggle={setIsOpen} direction="left" duration={0.9} />}
+        { windowSize >= 1025 ? <SideBar /> : 
+         <div className='hamburger'><Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={openNav} toggle={setOpenNav} direction="left" duration={0.9} /></div>}
       </div>
-      <div className='v-nav'>
+      {/* <div className='v-nav'>
         {windowSize < 1025 && isOpen && <SideBar />}
-      </div>
+      </div> */}
       
     </header>
   )
