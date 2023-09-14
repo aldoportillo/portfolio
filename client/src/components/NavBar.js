30365370c917d74bf4db35554d030c25250deb03
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-14 16:23:39 -0500

    Change Nav bar names

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+        <nav className='nav-bar'>
+            <Link to="/" >Home</Link>
+            <Link to="/projects" >Projects</Link>
+            <Link to="/blog" >Blog</Link>
+            <Link to="/contact" >Contact</Link> 
+        </nav>
+  )
+}
