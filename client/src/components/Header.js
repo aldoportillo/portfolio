97b50bf534c684ccf5680e3248717977fdf1d4ec
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-14 16:23:52 -0500

    Delete legacy code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { Sling as Hamburger } from 'hamburger-react'
-import SideBar from './SideBar'
 import { Link } from 'react-router-dom'
+import NavBar from './NavBar'
 
 export default function Header({openNav,  setOpenNav, windowSize}) {
 
   return (
     <header>
-      <div className='h-nav'>
         <h1><Link to="/">Aldo Portillo</Link></h1>
-        { windowSize >= 1025 ? <SideBar /> : 
+        { windowSize >= 1025 ? <NavBar /> : 
          <div className='hamburger'><Hamburger hideOutline={false} label="Open Menu" rounded size={38} toggled={openNav} toggle={setOpenNav} direction="left" duration={0.9} /></div>}
-      </div>
-      {/* <div className='v-nav'>
-        {windowSize < 1025 && isOpen && <SideBar />}
-      </div> */}
-      
     </header>
   )
 }
