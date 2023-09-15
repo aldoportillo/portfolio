c417cbc403db81501ef1a3b977d2f6b4b6f16321
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-15 16:19:55 -0500

    Call Dev.to API to get blogs

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Contact from './pages/Contact';
 import BlogEntry from './components/BlogEntry';
 import PageContainer from './components/PageContainer';
 import axios from 'axios';
+import NewBlogs from './pages/NewBlogs';
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
   const [projectData, setProjectData] = React.useState([])
   const [areBlogsLoading, setAreBlogsLoading] = React.useState(true)
   const [areProjectsLoading, setAreProjectsLoading] = React.useState(true)
+
+  const [devData, setDevData ] = React.useState([])
+  const [isDevLoading, setIsDevLoading] = React.useState(true)
 
 
   React.useEffect(() => {
@@ -29,6 +33,12 @@ function App() {
         setProjectData(res.data)
         
       })
+
+      axios.get(`https://dev.to/api/articles?username=aldoportillo`)
+      .then(res => {
+        setDevData(res.data)
+        setIsDevLoading(false)
+      })
   }, [])
 
   
@@ -38,9 +48,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<PageContainer children={<Home />}/>} />
         <Route path="/projects" element={<PageContainer children={<Projects isLoading={areProjectsLoading} projectData={projectData}/>} />} />
-        <Route path="/blog/" element={<PageContainer children={<Blog isLoading={areBlogsLoading} blogData={blogData}/>} />}/>
-        <Route path="/blog/:id" element={<PageContainer children={<BlogEntry />} />} />
+        <Route path="/blog1/" element={<PageContainer children={<Blog isLoading={areBlogsLoading} blogData={blogData}/>} />}/>
+        <Route path="/blog1/:id" element={<PageContainer children={<BlogEntry />} />} />
         <Route path="/contact" element={<PageContainer children={<Contact />} />} />
+        <Route path="/blog" element={<PageContainer children={<NewBlogs devData={devData} />} />} />
       </Routes>
     </div>
   );
