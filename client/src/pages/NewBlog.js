cd8839d3c2fa80b432bda661dbe24b0c429229aa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-16 20:21:24 -0500

    Style code blocks

--- client/src/pages/NewBlog.js
+++ client/src/pages/NewBlog.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {oneDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export default function NewBlog() {
     const location = useLocation()
@@ -19,7 +22,28 @@ export default function NewBlog() {
   return (
     <div className='blog'>
       <h1>{content.title}</h1>
-      <ReactMarkdown children={content.body_markdown} />
+
+      <ReactMarkdown
+        children={content.body_markdown}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, '')}
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            )
+          }
+        }} />
     </div>
   )
 }
